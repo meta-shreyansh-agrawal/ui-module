64d92b68937977ea9a964051ba09a800aadb4307
Shreyansh Agrawal <shreyansh.agrawal@metacube.com>
2025-05-13 15:25:25 +0530

    resolved bug: dragable issue with buttons

--- task-tracker/src/app/components/TaskCard.tsx
+++ task-tracker/src/app/components/TaskCard.tsx
@@ -20,20 +20,22 @@ const TaskCard: React.FC<TaskCardProps> = ({ task, onEditTask, onDeleteTask }) =
     const { attributes, listeners, setNodeRef } = useDraggable({ id: task.id.toString() });
 
   return (
-    <div ref={setNodeRef} {...attributes} {...listeners} className={`p-4 rounded ${priorityColors[task.priority]} text-white`}>
-      <div>
+    <div ref={setNodeRef}  className={`p-4 rounded ${priorityColors[task.priority]} text-white`}>
+      <div {...attributes} {...listeners}>
       <h3 className="text-lg font-semibold">{task.title}</h3>
       <p>{task.description}</p>
       <p className="text-sm">{task.status}</p>
       </div>
-      { task.status!="Completed" && <div>
-      <button onClick={() => onEditTask(task)} className="text-white hover:text-gray-300">
-        <FaEdit size={20} />
-      </button>
-      <button onClick={() => onDeleteTask(task.id)} className="text-white hover:text-gray-300">
-          <FaTrash size={20} />
-      </button>
-      </div>}
+      { task.status!="Completed" &&<div className="flex items-center space-x-2">
+  <button onClick={() => onEditTask(task)} className="text-white hover:text-gray-300">
+    <FaEdit size={20} />
+  </button>
+  <button onClick={() => onDeleteTask(task.id)} className="text-white hover:text-gray-300">
+    <FaTrash size={20} />
+  </button>
+  <div {...attributes} {...listeners} className="flex-1 bg-gray-200 h-full"></div> {/* Expands to fill remaining space */}
+</div>
+}
     </div>
     
   );
